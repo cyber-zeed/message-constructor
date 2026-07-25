29d72d611e356d259a24fdbf9a4e1ee30c518211
cyber-zeed <34373499+cyber-zeed@users.noreply.github.com>
2026-07-24 22:38:29 -0300

    Harden message constructor library

--- message_constructor.h
+++ message_constructor.h
@@ -1,69 +1,8 @@
-#ifndef MESSAGE_CONSTRUCTOR_H
-#define MESSAGE_CONSTRUCTOR_H
+#ifndef MESSAGE_CONSTRUCTOR_COMPAT_H
+#define MESSAGE_CONSTRUCTOR_COMPAT_H
 
-#ifdef ARDUINO
-#include <Arduino.h>  // Include Arduino header for millis() and String
-#else
-#include <string>
-#include <chrono>
-// Define millis() equivalent for non-Arduino platforms
-static inline unsigned long millis() {
-    auto now = std::chrono::high_resolution_clock::now();
-    auto duration = now.time_since_epoch();
-    return std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-}
-// Define String equivalent for non-Arduino platforms
-using String = std::string;
-// Define Arduino String-style constructor for numbers on non-Arduino platforms
-template<typename T>
-String StringFromNumber(T num) {
-    return std::to_string(num);
-}
-// Specialization for const char* to avoid converting string literals to numbers
-inline String StringFromNumber(const char* str) {
-    return String(str);
-}
-// Specialization for String type to avoid double conversion
-inline String StringFromNumber(const String& str) {
-    return str;
-}
+// Backward-compatible include. New projects should include
+// <MessageConstructor.h> from the library's src directory.
+#include "src/MessageConstructor.h"
+
 #endif
-
-class MessageConstructor {
-public:
-    // Constructor
-    MessageConstructor() {}
-
-    // Destructor
-    ~MessageConstructor() {}
-
-    // Function to construct the message with a variable number of arguments
-    template<typename... Args>
-    String constructMessage(Args&&... args) {
-#ifdef ARDUINO
-        String message = "@#<" + String(millis()) + ">#";
-#else
-        String message = "@#<" + StringFromNumber(millis()) + ">#";
-#endif
-        constructMessageHelper(message, std::forward<Args>(args)...); // Call helper function
-        message += "#@";
-        return message;
-    }
-
-private:
-    // Helper function to concatenate arguments
-    template<typename T, typename... Args>
-    void constructMessageHelper(String& message, T&& arg, Args&&... args) {
-#ifdef ARDUINO
-        message += "&" + String(arg);
-#else
-        message += "&" + StringFromNumber(arg);
-#endif
-        constructMessageHelper(message, std::forward<Args>(args)...); // Recur for remaining arguments
-    }
-
-    // Base case for the recursive helper function
-    void constructMessageHelper(String& message) {}
-};
-
-#endif /* MESSAGE_CONSTRUCTOR_H */
