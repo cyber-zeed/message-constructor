dc1ca946d81a47e7889d0d56d805a646e861e6f2
cyber-zeed <34373499+cyber-zeed@users.noreply.github.com>
2026-07-24 22:39:05 -0300

    Add native regression tests

---
+++ tests/test_message_constructor.cpp
@@ -0,0 +1,18 @@
+#include "MessageConstructor.h"
+
+#include <cassert>
+#include <iostream>
+#include <string>
+
+int main() {
+    const MessageConstructor constructor;
+
+    assert(constructor.constructMessageAt(1000) == "@#<1000>#@ ".substr(0, 11));
+    assert(constructor.constructMessageAt(1000, "hello") == "@#<1000>#&hello#@");
+    assert(constructor.constructMessageAt(42, 'A', true, -7, 25.5) == "@#<42>#&A&1&-7&25.5#@");
+    assert(constructor.constructMessageAt(5, "A&B", "x#y", "c\\d") ==
+           "@#<5>#&A\\&B&x\\#y&c\\\\d#@");
+
+    std::cout << "All MessageConstructor tests passed.\n";
+    return 0;
+}
